c8ec895ea3b42c62889ebe6a9b4c4bd12542f23f
john11dark <godsaveme2001@gmail.com>
2022-06-23 03:45:47 +0200

    Auth Commit

--- App/components/Lists/ListItem.js
+++ App/components/Lists/ListItem.js
@@ -3,77 +3,79 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
+  TouchableOpacity,
   View,
 } from "react-native";
 
 import customProps from "../../config/customProps";
 
-let initialAuthor = false;
 export default function ListItem({
   title,
   subTitle,
   imagePath,
   IconComponent,
-  style,
+  styleParameter,
   onPress,
 }) {
   return (
     // Images
-    <TouchableHighlight
+    <TouchableOpacity
       underlayColor={customProps.primaryColorDarkOpacity}
       onPress={onPress}
     >
-      <View style={[styles.container, style]}>
+      <View style={[styles.container, styleParameter]}>
         {IconComponent}
         {imagePath && <Image source={imagePath} style={styles.image} />}
 
         {/* Text */}
         <View style={styles.textContainer}>
-          <Text style={styles.title}>{title}</Text>
-          {subTitle && <Text style={styles.subTitle}>{subTitle}</Text>}
+          <Text style={[styles.title, styleParameter]}>{title}</Text>
+          {subTitle && (
+            <Text style={[styles.subTitle, styleParameter]}>{subTitle}</Text>
+          )}
         </View>
       </View>
-    </TouchableHighlight>
+    </TouchableOpacity>
   );
 }
 const styles = StyleSheet.create({
   image: {
-    width: initialAuthor ? 100 : 70,
-    height: initialAuthor ? 100 : 70,
+    width: 70,
+    height: 70,
     margin: 10,
-    borderRadius: initialAuthor ? 50 : 35,
+    borderRadius: 35,
     justifyContent: "center",
     alignItems: "center",
   },
 
   container: {
     backgroundColor: customProps.darkCardBackgroundColor,
-    //borderRadius: 10,
+    borderRadius: 10,
     margin: 1,
-    padding: 15,
-    paddingHorizontal: 20,
+    padding: 20,
     alignItems: "center",
     justifyContent: "flex-start",
     flexDirection: "row",
     width: "100%",
-    borderRadius: 15,
+    alignSelf: "center",
   },
 
   textContainer: {
     marginLeft: 25,
-    alignItems: "flex-Start",
+    alignItems: "flex-start",
+    width: "100%",
   },
 
   title: {
-    fontFamily: customProps.primaryFont,
+    fontFamily: customProps.font.fontFamily,
     fontSize: customProps.largePrimaryTextFontSize,
     color: customProps.primaryColorLight,
-    fontWeight: "600",
+    fontWeight: "500",
     marginBottom: 2,
     textTransform: "capitalize",
   },
   subTitle: {
-    fontFamily: customProps.primaryFont,
+    fontFamily: customProps.font.fontFamily,
     fontSize: customProps.mediumTextFontSize,
     color: customProps.primaryColorLightGray,
     textTransform: "capitalize",
